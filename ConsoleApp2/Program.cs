1f7591af10685258e7d45b7d28d13b681ec2807f
lipsa7 <lipsakar98@gmail.com>
2021-07-28 08:43:40 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static int factorial(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+        public static void Main(string[] args)
+        {
+            int n=1;
+            int tnum=0;
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 1){
+                while (factorial(n) < num)
+                {
+                    //int tnum;
+                    tnum = factorial(n);
+                    n++;
+                }
+            }
+            else
+            {
+                tnum = -1;
+            }
+
+            Console.WriteLine(tnum);
+            Console.ReadKey();
+            
+        }
+    }
+}
